5c890132ce410a34eec7d8bfbe0c6f4394f3b8c4
abnercasallo <68797376+abnercasallo@users.noreply.github.com>
2022-03-28 18:28:57 -0500

    readme y función para trimestre, año y días

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 #   SeriesBCRP
-This repository will contain a package to work with time series from BCRP
+This repository contains a package to work with time series from BCRP for daily, monthly, quarterly and annually. The data is provided 
+by API from BCRP, so in some cases we can find evidents outliers that need to be treated, specially in daily series. This outliers are commonly avoid in the data from web page of BCRP. 
+
+A good example of this is "PD04649MD" code ("Posición de cambio"). If you look this series in webpage, the december 23 and 24, 2000, you can't find the data, but the API gives "24989.173" for both dates, that are outliers and need to be cleaned. Anyway, they are excepcional cases of daily series and it will be evident when you plot the time series. 
+
+## Modules:
+### 1. Data Module
+#### 1.1. Import Data Module
+You can import this module using "from seriesbcrp2 import data"
+##### 1.2. Functions of Data Module
+Data module contain the next functions:
+A. bcrp function: This function call a data frame according the code of the serie and period of analysis.
+Remember that quarterly codes finish with "Q" and monthly codes finish with "M". It's important to call the period in the correct way. 
+See this examples to understand better:
+
+-If you want the daily data of "Posición de cambio" serie from January 1, 2000 to December 31, 2020, you have to write data.bcrp("PD04649MD", "/2000-1-1/2019-12-31").
+
+-If you want the PBI data serie from 2000 to 2021 by months, you have to use: "data.bcrp("PN01770AM", "/2000-1/2019-12"), where 
+"PN01770AM" is the code for monthly serie of PBI and "/2000-1/2019-12" the choosen period.
+
+-If you want the previous PBI series, but quarterly, you have to use: data.bcrp("PN39029BQ", "/2000-1/2019-3"), where "2000-1" means first quarter of 2000 and "2019-3" means third quarter of 2019. In this case, BCRP only offers data from 2018, so the result will be according this. 
+
+- If you want the annual data of "Activos externos netos de corto plazo" from 2005 to 2020, you have to use data.bcrp("PM06069MA", "/2005/2019")
+
+B. The "graph" function: This function allows to create a graph only with code and period inputs (similar to bcrp function).
+You can choose the title and axis names according a required input.
+
+C. The "boxplot" function: This function allows to create a boxplot only with code and period inputs (similar to bcrp function).
+You can choose the title name according a required input.
+
+D. The "histogram" function: This function allows to create a histogram only with code and period inputs (similar to bcrp function).
+You can choose the title name according a required input.
+
+E. The hep_code function: This function show you the link to codes of series of BCRP.
--- src/seriesbcrp2/data.py
+++ src/seriesbcrp2/data.py
@@ -5,15 +5,18 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 def bcrp (code,period): #código y periodo de la serie (en formato tipo /2005-1/2019-12)
+    ##Primero llamamos los datos del API del BCRP
     url_base="https://estadisticas.bcrp.gob.pe/estadisticas/series/api/"
-    formato="/json"
-    url=url_base+code+formato+period
+    Formato="/json"
+    url=url_base+code+Formato+period
+    import requests
     response=requests.get(url)
     response_json=response.json()
     periodos=response_json.get("periods")
     price_index=[]
-    for i in periodos: 
+    for i in periodos:
         valores_list=i['values']
         for w in valores_list:
             w=float(w)
@@ -22,52 +25,181 @@ def bcrp (code,period): #código y periodo de la serie (en formato tipo /2005-1/
     for i in periodos:
         nombres=i['name']
         fechas.append(nombres)
-    #Limpieza y creación del DataFrame
-    #Cambiamos los meses recortados por números para que la librería datatime reconozca el formato
-    fechas2=[]
-    for j in fechas:
-        r=j.split(".")
-        if r[0]=='Ene':
-            r[0]='01-'
-        if r[0]=='Feb':
-            r[0]='02-'
-        if r[0]=='Mar':
-            r[0]='03-'
-        if r[0]=='Abr':
-            r[0]='04-'
-        if r[0]=='May':
-            r[0]='05-'
-        if r[0]=='Jun':
-            r[0]='06-'
-        if r[0]=='Jul':
-            r[0]='07-'
-        if r[0]=='Ago':
-            r[0]='08-'
-        if r[0]=='Sep':
-            r[0]='09-'
-        if r[0]=='Oct':
-            r[0]='10-'
-        if r[0]=='Nov':
-            r[0]='11-'
-        if r[0]=='Dic':
-            r[0]='12-'           
-        fechas2.append(r)
-    fechas3=[]
-    for r in fechas2:
-        s=r[0]+r[1]
-        fechas3.append(s)
+    #Limpieza y creación del DataFrame.
+    #Habrán un bloque para datos mensuales y otro para trimestrales, ya que el formato del API
+    #del BCRP es diferente para cada caso
+    #En los datos mensuales se usará la librería datatime para convertir las fechas de string a dates
+    #Para los datos trimestrales se usará directamente la función datatime de pandas, se podría hacer con la librería anterior también.
+    ####PARA DATA MENSUAL (EL CÓDIGO ACABA EN M)
+    if code[-1]=="M":
+        fechas2=[]
+        for j in fechas:    #Cambiamos los meses recortados por números para que la librería datatime reconozca el formato
+            r=j.split(".")
+            if r[0]=='Ene':
+                r[0]='01-'
+            if r[0]=='Feb':
+                r[0]='02-'
+            if r[0]=='Mar':
+                r[0]='03-'
+            if r[0]=='Abr':
+                r[0]='04-'
+            if r[0]=='May':
+                r[0]='05-'
+            if r[0]=='Jun':
+                r[0]='06-'
+            if r[0]=='Jul':
+                r[0]='07-'
+            if r[0]=='Ago':
+                r[0]='08-'
+            if r[0]=='Sep':
+                r[0]='09-'
+            if r[0]=='Oct':
+                r[0]='10-'
+            if r[0]=='Nov':
+                r[0]='11-'
+            if r[0]=='Dic':
+                r[0]='12-'           
+            fechas2.append(r)
+            fechas3=[]
+        for r in fechas2:
+            s=r[0]+r[1]
+            fechas3.append(s)
     
-    fechas_final=[]
-    for i in fechas3:
-        t = datetime.strptime(i, '%m-%Y')
-        fechas_final.append(t)
-    #Creamos el diccionario
-    diccionario= {"Fechas":fechas_final, "Valores":price_index}
+        from datetime import datetime #Una vez que tenemos el formato compatible, procedemos a usar datatime
+        fechas_final=[]
+        for i in fechas3:
+            t = datetime.strptime(i, '%m-%Y')
+            fechas_final.append(t)
+    #Creamos el diccionario 
+        diccionario= {"Fechas":fechas_final, "Valores":price_index}
     #Creamos el dataframe  
-    df = pd.DataFrame(diccionario)
+        import pandas as pd
+        df = pd.DataFrame(diccionario)
+    ###PARA DATA TRIMESTRAL (EL CÓDIGO ACABA EN Q)
+    elif code[-1]=="Q":
+        fechas2_t=[]
+        for j in fechas: #Cambiamos los trimestres y años para que pandas (datatime) reconozca el formato
+            r=j.split(".")
+            if r[0]=='T1':
+                r[0]='Q1'
+            if r[0]=='T2':
+                r[0]='Q2'
+            if r[0]=='T3':
+                r[0]='Q3'
+            if r[0]=='T4':
+                r[0]='Q4' 
+            q=list(r[1])
+            if q[0]=="5":
+                q[0]="195"
+            if q[0]=="6":
+                q[0]="196"
+            if q[0]=="7":
+                q[0]="197"
+            if q[0]=="8":
+                q[0]="198"
+            if q[0]=="9":
+                q[0]="199"
+            if q[0]=="0":
+                q[0]="200"
+            if q[0]=="1":
+                q[0]="201"
+            if q[0]=="2":
+                q[0]="202"
+            if q[0]=="3":
+                q[0]="203"
+            if q[0]=="4":
+                q[0]="204"
+            s="".join(q)
+            r[1]=s
+            r[1],r[0]=r[0],r[1]  ###Intercambio el orden. Es necesario para usar la función datatime de pandas
+            fechas2_t.append(r)  
+        #Junto los datos trimestrales
+        fechas3_t=[]
+        for r in fechas2_t:
+            s=r[0]+r[1]
+            fechas3_t.append(s)
+        diccionario= {"Fechas":fechas3_t, "Valores":price_index}
+        import pandas as pd
+        df = pd.DataFrame(diccionario)
+        df.Fechas = pd.to_datetime(df.Fechas)  #Aplico directamente la función datatime de pandas
+    elif code[-1]=="A":
+        diccionario= {"Fechas":fechas, "Valores":price_index}
+        df = pd.DataFrame(diccionario)
+        df.Fechas = pd.to_datetime(df.Fechas)
+    elif code[-1]=="D":
+        lista=[]
+        for i in fechas:
+            a=i.replace(".", "-")
+            lista.append(a)
+        fechas2=[]
+        for j in lista:
+            r=j.split("-")
+            if r[1]=='Ene':
+                r[1]='01-'
+            if r[1]=='Feb':
+                r[1]='02-'
+            if r[1]=='Mar':
+                r[1]='03-'
+            if r[1]=='Abr':
+                r[1]='04-'
+            if r[1]=='May':
+                r[1]='05-'
+            if r[1]=='Jun':
+                r[1]='06-'
+            if r[1]=='Jul':
+                r[1]='07-'
+            if r[1]=='Ago':
+                r[1]='08-'
+            if r[1]=='Set': ##Acá lo han puesto como setiembre, en el mensual está como sep
+                r[1]='09-'
+            if r[1]=='Oct':
+                r[1]='10-'
+            if r[1]=='Nov':
+                r[1]='11-'
+            if r[1]=='Dic':
+                r[1]='12-'  
+            q=list(r[2])
+            if q[0]=="5":
+                q[0]="195"
+            if q[0]=="6":
+                q[0]="196"
+            if q[0]=="7":
+                q[0]="197"
+            if q[0]=="8":
+                q[0]="198"
+            if q[0]=="9":
+                q[0]="199"
+            if q[0]=="0":
+                q[0]="200"
+            if q[0]=="1":
+                q[0]="201"
+            if q[0]=="2":
+                q[0]="202"
+            if q[0]=="3":
+                q[0]="203"
+            if q[0]=="4":
+                q[0]="204"
+            s="".join(q)
+            r[2]=s
+    #r[-1],r[0]=r[0],r[-1]
+            fechas2.append(r)
+        fechas3=[]
+        for r in fechas2:
+            s=r[0]+"-"+r[1]+r[2]
+            fechas3.append(s)
+        final=[]
+        for i in fechas3:
+            t = datetime.strptime(i, '%d-%m-%Y')
+            final.append(t)
+        diccionario= {"Fechas":final, "Valores":price_index}
+        df = pd.DataFrame(diccionario)
     return df
 
-def histograma (code,period):
+    return df
+
+
+
+def histogram (code,period):
     df=bcrp (code,period)
     title=input('Título que desea que tenga el histograma:')
     plt.title(title)
@@ -89,7 +221,15 @@ def graph(code, period):
     plt.ylabel(y)
     return df.plot(x ='Fechas', y='Valores', figsize=(15, 5), kind = 'line')
 
-    boxplot("PN01770AM", "/2005-1/2019-12")
-    
+def help_code():
+    x="Puedes encontrar los códigos de las series del BCRP en el siguiente enlace: https://estadisticas.bcrp.gob.pe/estadisticas/series/anuales"
+    return x
+#PD04649MD /1999-1-1/2019-12-31
+#boxplot("PN02561AQ", "/2005-1/2019-12")
+
+#boxplot("PD04649MD ", "/1999-1-1/2019-12-31")
+
+
+
     #x = input('Nombre del Eje X:')
     #y = input('Nombre del Eje Y:')
